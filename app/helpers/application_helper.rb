f910909ca6d7f3b549fb21dfc69edb9acd487eb5
alexis finch <agent.fin@gmail.com>
2012-08-04 19:11:05 -0700

    Add custom css and adjust layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  # Returns the full title for the page
+  
+  def full_title(page_title)
+    base_title = "Zee Blogger"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
 end
