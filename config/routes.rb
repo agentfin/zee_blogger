31ef540262744d75289bced63d6cb05e4829e96a
alexis finch <agent.fin@gmail.com>
2012-08-06 15:33:45 -0700

    Sets tags for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 ZeeBlogger::Application.routes.draw do
+  get "tag/index"
+
+  get "tag/show"
+
   root :to => 'articles#index'
   
-  resources :articles, :comments
+  resources :articles, :comments, :tags, :taggings
 
 end
